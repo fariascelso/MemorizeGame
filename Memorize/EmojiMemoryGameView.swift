f9c5176b7aeb011dd7b5df02fead36b13b4a0a25
Celso Farias <c__farias@outlook.com>
2025-01-27 18:41:32 -0300

    feat: clean project

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -26,18 +26,23 @@ struct EmojiMemoryGameView: View {
     }
     
     var cards: some View {
-        LazyVGrid(columns: [GridItem(.adaptive(minimum: 85), spacing: 0)], spacing: 0) {
-            ForEach(viewModel.cards) { card in
+        GeometryReader { geometry in
+
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 95), spacing: 0)], spacing: 0) {
+                ForEach(viewModel.cards) { card in
                     CardView(card)
                         .aspectRatio(2/3, contentMode: .fit)
                         .padding(4)
                         .onTapGesture {
                             viewModel.choose(card)
                         }
+                }
             }
         }
         .foregroundColor(.pink)
     }
+
+    
 }
 
 struct CardView: View {
